f320a69945ced16e4423b9e4b2cb64371f687853
Jeeva Kalaiselvam <jeevak001@outlook.com>
2021-04-23 09:56:43 +0530

    Repo list obtained

--- script.js
+++ script.js
@@ -4,14 +4,22 @@ const REPOSAPI = "https://api.github.com/users/repos";
 const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
-
-async function getUserData(user = "florinpop17") {
-    const response = await fetch(APIURL + user);
-    const responseData = await response.json();
-    createUserCard(responseData);
-}
+const lastSearch = "florinpop17";
 
 getUserData();
+
+async function getUserData(username = "florinpop17") {
+    const response = await fetch(APIURL + username);
+    const responseData = await response.json();
+    createUserCard(responseData);
+    getRepos(username);
+}
+
+async function getRepos(username) {
+    const response = await fetch(APIURL + username + "/repos");
+    const responseData = await response.json();
+    addReposToCard(responseData);
+}
 
 function createUserCard(user) {
     const cardHTML = `
@@ -26,18 +34,37 @@ function createUserCard(user) {
             <li>${user.followers}<strong>Followers</strong></li>
             <li>${user.following}<strong>Following</strong></li>
             <li>${user.public_repos}<strong>Repos</strong></li>
-        </ul
+        </ul>
+
+        <div id="repos" class="repos">
+        </div>
     </div>
+     
     </div>
     `;
 
     main.innerHTML = cardHTML;
 }
 
+function addReposToCard(repos) {
+    const reposEl = document.getElementById("repos");
+    console.log(reposEl);
+    console.log(repos);
+    repos.forEach((repo) => {
+        const repoEl = document.createElement("a");
+        repoEl.classList.add("repo");
+        repoEl.href = repo.html_url;
+        repoEl.target = "_blank";
+        repoEl.innerText = repo.name;
+        reposEl.appendChild(repoEl);
+    });
+}
+
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const user = search.value;
+    lastSearch = search.value;
 
     if (user) {
         getUserData(user);
